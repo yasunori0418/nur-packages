79260ec4d4b1b3ac34caf264aeaf3619070f0a3d
yasunori0418 <yasunori.kirin0418@gmail.com>
2025-09-15 01:39:34 +0900

    update: refer vimPlugins.nvim-treesitter-parsers

--- pkgs/nvim-treesitter-parsers/default.nix
+++ pkgs/nvim-treesitter-parsers/default.nix
@@ -1,39 +1,14 @@
 {
-  lib,
-  stdenv,
-  tree-sitter-grammars,
+  symlinkJoin,
+  vimPlugins,
   ...
 }:
 let
-  inherit (builtins) toString;
-  inherit (lib) concatStringsSep foldl;
-
-  baseOutDirectory = "mkdir -p $out/{parser,queries}";
-
-  genQueriesDirectory = foldl (acc: parser: acc + "mkdir -p $out/queries/${parser}\n") "";
-
-  genTreeSitterLinks = foldl (
-    acc: parser:
-    let
-      name = parser;
-      store = toString tree-sitter-grammars."tree-sitter-${parser}";
-    in
-    acc
-    + ''
-      ln -svf ${store}/parser $out/parser/${name}.so
-      ln -svf ${store}/queries/* $out/queries/${name}/
-    ''
-  ) "";
-
   nvim-treesitter-parsers =
     parsers:
-    stdenv.mkDerivation {
+    symlinkJoin {
       name = "nvim-treesitter-parsers";
-      buildCommand = concatStringsSep "\n" [
-        baseOutDirectory
-        (genQueriesDirectory parsers)
-        (genTreeSitterLinks parsers)
-      ];
+      paths = map (parser: vimPlugins.nvim-treesitter-parsers.${parser}) parsers;
     };
 in
 nvim-treesitter-parsers
