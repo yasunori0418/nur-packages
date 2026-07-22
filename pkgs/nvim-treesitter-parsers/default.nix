2605d945488d9501a2cec23dfceb7ade5d8144d5
yasunori0418 <yasunori.kirin0418@gmail.com>
2026-07-22 17:28:10 +0900

    Revert "feat(nvim-treesitter-parsers): nvim-treesitter 同梱の queries を出力に追加"
    This reverts commit ca910060cff5d6b2b489d7ed229392f2e948fc12.

--- pkgs/nvim-treesitter-parsers/default.nix
+++ pkgs/nvim-treesitter-parsers/default.nix
@@ -4,17 +4,11 @@
   ...
 }:
 let
-  # queries は parser と同一 nixpkgs スナップショットの nvim-treesitter 本体から
-  # 供給する。parser rev とクエリの整合が nixpkgs 側で保証されるため、
-  # 別経路(プラグインマネージャの HEAD 追従)由来のクエリとの node type 不一致を防ぐ。
   nvim-treesitter-parsers =
     parsers:
     symlinkJoin {
       name = "nvim-treesitter-parsers";
       paths = map (parser: vimPlugins.nvim-treesitter-parsers.${parser}) parsers;
-      postBuild = ''
-        ln -s ${vimPlugins.nvim-treesitter}/runtime/queries $out/queries
-      '';
     };
 in
 nvim-treesitter-parsers
