4a480d49073d33b579b49350b3362be282ff4ba0
yasunori0418 <yasunori.kirin0418@gmail.com>
2026-09-17 22:33:45 +0900

    update: 最新のdenoを取るように変更

--- pkgs/deno-overlay/default.nix
+++ pkgs/deno-overlay/default.nix
@@ -25,7 +25,4 @@ in
 # 網羅しているので通常このフォールバックには入らないが、上流が arch を
 # 落とした場合に評価全体を壊さないよう null を返す。
 # null は lib.isDerivation フィルタで packages 出力から除外される。
-# if latestVersion == "" then null else denoVersions.${latestVersion}
-
-# 一時的に2.9.5にダウングレード
-denoVersions."2.9.5"
+if latestVersion == "" then null else denoVersions.${latestVersion}
